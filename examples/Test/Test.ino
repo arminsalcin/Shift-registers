0799bedd3f6a7bd1d0a2f4c2978d117ea79c89c2
Armin Salčin <arminsalcin@users.noreply.github.com>
2017-03-14 09:59:03 +0100

    Update Test.ino

--- examples/Test/Test.ino
+++ examples/Test/Test.ino
@@ -1,12 +1,12 @@
 #include <shift.h>
 
-Shift Shift (16,5,4,1);
+Shift Shift (16,5,4,1);// Shift(SER_Pin,RCLK_Pin,SRCLK_Pin,NUM_Shift);
 void setup() {
  
 }
 
 void loop() {
 
-Shift.setRegister(5,1);
+Shift.setRegister(5,1); //turn HIGH pin 5 on shift register
 
 }
